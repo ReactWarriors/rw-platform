e3065d18f087427103e617fb425a99c4b76a7ee0
Alex Komar <909komar@gmail.com>
2020-06-15 01:46:56 +0300

    add support sass

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,27 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { join } from 'path';
 import * as hbs from 'hbs';
+import * as sassMiddleware from 'node-sass-middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
   hbs.registerPartials(join(__dirname, '..', 'views/partials'));
+  app.use(
+    sassMiddleware({
+      src: join(__dirname, '..', 'src'), //where the styles files are
+      dest: join(__dirname, '..', 'public'), //where styles should go
+      prefix: '',
+      sourceMap: true,
+      outputStyle: 'compressed',
+      indentedSyntax: false,
+      force: true,
+      debug: true, // obvious
+    }),
+  );
+  app.useStaticAssets(join(__dirname, '..', 'public'));
   await app.listen(3000);
 }
+
 bootstrap();
