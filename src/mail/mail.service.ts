b46069ca09e20c8f92e0e7395f19d5ba66a68c69
Alex Komar <909komar@gmail.com>
2020-12-26 00:17:31 +0200

    fix mailer

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -2,25 +2,26 @@ import { Inject, Injectable, Logger } from '@nestjs/common';
 import * as nodemailer from 'nodemailer';
 import { ISendEmailData } from './interfaces/sendEmailData.interface';
 
-const logger = new Logger('MailService');
-
 @Injectable()
 export class MailService {
+  logger: Logger;
+
   constructor(@Inject('MAILER_CONNECTION') private readonly transport) {
+    this.logger = new Logger('MailService');
   }
 
   async send(data: ISendEmailData) {
     try {
       const info = await this.transport.sendMail(data);
-      logger.log(`${data.subject} email was send to: ${data.to}`)
+      this.logger.log(`${data.subject} email was send to: ${data.to}`);
 
-      logger.log('Message sent: %s', info.messageId);
+      this.logger.log(`Message sent: ${info.messageId}`);
 
       if (process.env.NODE_ENV === 'development' && !process.env.DEBUG_MAILER) {
-        logger.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+        this.logger.log(`Preview URL: ${nodemailer.getTestMessageUrl(info)}`);
       }
     } catch (e) {
-      logger.error(e)
+      this.logger.error(e);
     }
   }
 }
